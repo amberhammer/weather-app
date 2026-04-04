f470e7849756969c6e227edc4aff7d60609be869
amberhammer <amberhammer@hotmail.com>
2026-04-04 13:49:53 -0700

    Basic styling for hourly forecast

--- src/components/Hourly.jsx
+++ src/components/Hourly.jsx
@@ -1,19 +1,33 @@
+import styles from './Hourly.module.css'
+
 export default function Hourly( {weatherData} ) {
   return (
-    <div className="hourly">
+    <div className={styles['hourly']}>
       <h2>Hourly Forecast</h2>
-      <div className="hourly-boxes">
+      <div className={styles['hourly-boxes']}>
         {weatherData.hourly.slice(0, 24).map((hour, index) => (
-          <div key={index} className="hourly-item">
-            <p>{new Date(hour.dt * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
-            <img src={`http://openweathermap.org/img/wn/${hour.weather[0].icon}.png`} alt={hour.weather[0].description} />
-            <p>{hour.temp}°C</p>
-            <p>{hour.weather[0].description}</p>
-            <p>Feels Like: {hour.feels_like}°C</p>
-            <p>Wind: {hour.wind_speed} mph</p>
-            <p>Wind Gust: {hour.wind_gust} mph</p>
-            <p>Humidity: {hour.humidity}%</p>
-            <p>Precipitation: {hour.pop * 100}%</p>
+          <div key={index} className={styles['hourly-item']}>
+            <div className={styles['hourly-time']}>
+              <p>{new Date(hour.dt * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+            </div>
+            <div className={styles['hourly-main']}>
+              <div className={styles['hourly-icon-temp']}>
+                <img src={`http://openweathermap.org/img/wn/${hour.weather[0].icon}.png`} alt={hour.weather[0].description} />
+                <p>{hour.temp}°C</p>
+              </div>
+              <div className={styles['hourly-description']}>
+                <p>{hour.weather[0].description}</p>
+                <p>Feels Like: {hour.feels_like}°C</p>
+              </div>
+              <div className={styles['hourly-details']}>
+                <p>Wind: {hour.wind_speed} mph</p>
+                <p>Wind Gust: {hour.wind_gust} mph</p>
+                <p>Humidity: {hour.humidity}%</p>
+              </div>
+              <div className={styles['hourly-pop']}>
+                <p>Precipitation: {hour.pop * 100}%</p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
