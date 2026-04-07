6f5ab66390bf59b58659d1c434d1048acb012b13
amberhammer <amberhammer@hotmail.com>
2026-04-06 20:49:41 -0700

    Testing feather icons

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,11 +1,11 @@
 import styles from './Alert.module.css';
 import { formatTimeInTimezone } from '../utils/dateUtils';
-// import { Alert-Triangle } from 'react-feather';
+import { AlertTriangle } from 'react-feather';
 
 export default function Alert( {alert, timezoneOffset} ) {
   return (
     <div className={styles.alert}>
-      {/* <Alert-Triangle size={24} color="#ff4d4d" /> */}
+      <AlertTriangle size={18} color="var(--text)" />
       <p><strong>{alert?.[0]?.sender_name}:</strong> {alert?.[0]?.description.split('.')[0]} from {formatTimeInTimezone(alert?.[0]?.start, timezoneOffset, { hour: 'numeric', minute: '2-digit' })} to {formatTimeInTimezone(alert?.[0]?.end, timezoneOffset, { hour: 'numeric', minute: '2-digit' })}</p>
     </div>
   )
